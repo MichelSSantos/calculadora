7b54f43792c84075959cc113109d2ecd7fe125bc
MichelSSantos <michelshaiene2010@hotmail.com>
2023-05-22 13:15:00 -0300

    Calculadora

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: 1px solid #CDCDCD;
+    background-color: #E6911F;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-height: 700;
+    flex: 1;
+
+    &:hover{
+        opacity: 0.6 ;
+    }
+`
